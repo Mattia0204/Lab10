55599b2d6595fbac78e1ede84a074f5d3576b4dc
mattia <mattiamogliotti@gmail.com>
2025-11-30 20:10:47 +0100

    prova

--- UI/controller.py
+++ UI/controller.py
@@ -16,5 +16,19 @@ class Controller:
         * Numero di Tratte
         * Lista di Tratte che superano il costo indicato come soglia
         """
-        # TODO
+        try:
+            guadagno_medio_minimo = float(self._view.guadagno_medio_minimo.value)
+        except (ValueError, TypeError) as e:
+            self._view.show_alert("Bisogna inserire un valore intero o float come guadagno medio minimo")
+            return
+        G = self._model.costruisci_grafo(guadagno_medio_minimo)
 
+        self._view.lista_visualizzazione.controls.clear()
+        self._view.lista_visualizzazione.controls.append(ft.Text(f"Numero di Hubs: {self._model.get_num_nodes()}"))
+        self._view.lista_visualizzazione.controls.append(ft.Text(f"Numero di tratte: {self._model.get_num_edges()}"))
+
+        i=1
+        for u, v, d in G.edges(data=True):
+            self._view.lista_visualizzazione.controls.append(ft.Text(f"{i}) [{u} -> {v}] -- Guadagno medio per spedizione €{d.get('weight')})"))
+            i+=1
+        self._view.lista_visualizzazione.update()
--- model/model.py
+++ model/model.py
@@ -12,26 +12,65 @@ class Model:
         Costruisce il grafo (self.G) inserendo tutti gli Hub (i nodi) presenti e filtrando le Tratte con
         guadagno medio per spedizione >= threshold (euro)
         """
-        # TODO
+        self.G = nx.Graph()
+        edges_valore = self.get_all_edges()
+        for edge, valore in edges_valore.items():
+            valore = float(valore)
+            if valore >= threshold:
+                nodo_1, nodo_2 = edge
+                self.G.add_edge(nodo_1, nodo_2, weight=valore)
+        return self.G
 
     def get_num_edges(self):
         """
         Restituisce il numero di Tratte (edges) del grafo
         :return: numero di edges del grafo
         """
-        # TODO
+        n=0
+        for elements in self.G.edges():
+            n+=1
+        self._edges = n
+        return self._edges
 
     def get_num_nodes(self):
         """
         Restituisce il numero di Hub (nodi) del grafo
         :return: numero di nodi del grafo
         """
-        # TODO
+        lista_hub=DAO.get_hub()
+        lista_hub_distinte = []
+        for hub in lista_hub.values():
+            if hub.id not in lista_hub_distinte:
+                lista_hub_distinte.append(hub.id)
+        self._nodes = len(lista_hub_distinte)
+        return self._nodes
 
     def get_all_edges(self):
         """
         Restituisce tutte le Tratte (gli edges) con i corrispondenti pesi
         :return: gli edges del grafo con gli attributi (il weight)
         """
-        # TODO
+        lista_spedizioni = DAO.get_spedizione()
+        lista_spedizioni_distinte = []
+        id_edges = {}
+        risultato = {}
+        lista_spedizioni_finale = {}
+        for spedizione in lista_spedizioni.values():
+            tratta = (spedizione.id_hub_origine, spedizione.id_hub_destinazione)
+            valore_corrente = float(spedizione.valore_merce)
+            if tratta not in lista_spedizioni_distinte:
+                lista_spedizioni_distinte.append(tratta)
+                risultato[tratta] = valore_corrente
+                id_edges[tratta] = 1
+            else:
+                valore_parziale = risultato[tratta]
+                num_edges = id_edges[tratta] + 1
+                valore_parziale = valore_parziale + valore_corrente
+                risultato[tratta] = valore_parziale
+                id_edges[tratta] = num_edges
+        for tratta, valore in risultato.items():
+            num_edges = id_edges[tratta]
+            valore_finale = valore / num_edges
+            lista_spedizioni_finale[tratta] = valore_finale
 
+        return lista_spedizioni_finale
